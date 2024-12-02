ed9d876df63f5642dd5e01fdc235fb47b9f71579
TrojanerHD <github@trojaner.dev>
2024-12-02 09:46:08 +0100

    Attempt to make day 2 part 2 a little prettier

--- src/day2.rs
+++ src/day2.rs
@@ -1,25 +1,33 @@
 use std::cmp::Ordering;
 
+#[derive(PartialEq)]
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
 pub fn part1(input: &str) -> String {
     let lines = input.lines();
     let mut res = 0;
     'lines: for line in lines {
         let mut prev: Option<u32> = None;
-        let mut increasing: i8 = 0;
+        let mut direction = None;
         for report in line.split_whitespace().map(|it| it.parse::<u32>().unwrap()) {
             if let Some(act_prev) = prev {
                 if report.abs_diff(act_prev) > 3
                     || report == act_prev
-                    || (increasing == 1 && report < act_prev)
-                    || (increasing == -1 && report > act_prev)
+                    || (direction == Some(Direction::Increasing) && report < act_prev)
+                    || (direction == Some(Direction::Decreasing) && report > act_prev)
                 {
                     continue 'lines;
                 }
-                if increasing == 0 {
-                    match report.cmp(&act_prev) {
-                        Ordering::Greater => increasing = 1,
-                        Ordering::Less => increasing = -1,
-                        Ordering::Equal => {}
+                if direction.is_none() {
+                    direction = match report.cmp(&act_prev) {
+                        Ordering::Greater => Some(Direction::Increasing),
+                        Ordering::Less => Some(Direction::Decreasing),
+                        Ordering::Equal => {
+                            panic!("report {} must be different from prev {}", report, act_prev)
+                        }
                     }
                 }
             }
@@ -31,17 +39,14 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    let lines = input
-        .lines()
-        .map(|line| {
-            line.split_whitespace()
-                .map(|it| it.parse::<u32>().unwrap())
-                .collect::<Vec<u32>>()
-        })
-        .collect::<Vec<Vec<u32>>>();
-    let mut res = 0;
-    let mut corr_lines: Vec<(usize, Vec<u32>)> = Vec::new();
-    for (i, line) in lines.iter().enumerate() {
+    let lines = input.lines().map(|line| {
+        line.split_whitespace()
+            .map(|it| it.parse::<u32>().unwrap())
+            .collect::<Vec<_>>()
+    });
+    let mut res: u32 = 0;
+    let mut corr_lines: Vec<(usize, _)> = Vec::new();
+    for (i, line) in lines.enumerate() {
         corr_lines.push((i, line.clone()));
         for report in 0..line.len() {
             corr_lines.push((
@@ -54,27 +59,29 @@ pub fn part2(input: &str) -> String {
             ))
         }
     }
-    let mut taken: Vec<_> = Vec::new();
+    let mut taken = Vec::new();
     'line: for (i, line) in corr_lines {
         if taken.contains(&i) {
             continue;
         }
         let mut prev = None;
-        let mut increasing: i8 = 0;
+        let mut direction = None;
         for report in line.clone() {
             if let Some(act_prev) = prev {
                 if report.abs_diff(act_prev) > 3
                     || report == act_prev
-                    || (increasing == 1 && report < act_prev)
-                    || (increasing == -1 && report > act_prev)
+                    || (direction == Some(Direction::Increasing) && report < act_prev)
+                    || (direction == Some(Direction::Decreasing) && report > act_prev)
                 {
                     continue 'line;
                 }
-                if increasing == 0 {
-                    match report.cmp(&act_prev) {
-                        Ordering::Greater => increasing = 1,
-                        Ordering::Less => increasing = -1,
-                        Ordering::Equal => {}
+                if direction.is_none() {
+                    direction = match report.cmp(&act_prev) {
+                        Ordering::Greater => Some(Direction::Increasing),
+                        Ordering::Less => Some(Direction::Decreasing),
+                        Ordering::Equal => {
+                            panic!("report {} must be different from prev {}", report, act_prev)
+                        }
                     }
                 }
             }
