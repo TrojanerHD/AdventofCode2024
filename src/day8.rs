7bff2283b15ffba190d05111f822b046ea96456a
TrojanerHD <github@trojaner.dev>
2024-12-08 07:47:11 +0100

    Clean up code a bit

--- src/day8.rs
+++ src/day8.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
 struct Point {
     x: u32,
     y: u32,
@@ -49,7 +49,7 @@ struct Antenna {
     letter: char,
 }
 
-pub fn part1(input: &str) -> String {
+fn parse_input(input: &str) -> (Vec<Antenna>, Point) {
     let points = input
         .lines()
         .enumerate()
@@ -69,13 +69,17 @@ pub fn part1(input: &str) -> String {
             })
         })
         .collect::<Vec<_>>();
-
     let max = Point {
         y: input.lines().count() as u32,
         x: input.lines().next().unwrap().chars().count() as u32,
     };
+    (points, max)
+}
 
+pub fn part1(input: &str) -> String {
+    let (points, max) = parse_input(input);
     let mut antinodes = HashSet::<Point>::new();
+
     for (i, antenna) in points.iter().enumerate() {
         for (j, antenna2) in points.iter().enumerate() {
             if i >= j || antenna.letter != antenna2.letter {
@@ -109,32 +113,9 @@ pub fn part1(input: &str) -> String {
 }
 
 pub fn part2(input: &str) -> String {
-    let points = input
-        .lines()
-        .enumerate()
-        .flat_map(move |(y, chars)| {
-            chars.chars().enumerate().filter_map(move |(x, pos)| {
-                if pos != '.' {
-                    Some(Antenna {
-                        point: Point {
-                            x: x.try_into().unwrap(),
-                            y: y.try_into().unwrap(),
-                        },
-                        letter: pos,
-                    })
-                } else {
-                    None
-                }
-            })
-        })
-        .collect::<Vec<_>>();
-
-    let max = Point {
-        y: input.lines().count() as u32,
-        x: input.lines().next().unwrap().chars().count() as u32,
-    };
-
+    let (points, max) = parse_input(input);
     let mut antinodes = HashSet::<Point>::new();
+
     for (i, antenna) in points.iter().enumerate() {
         antinodes.insert(antenna.point);
         for (j, antenna2) in points.iter().enumerate() {
@@ -150,6 +131,7 @@ pub fn part2(input: &str) -> String {
                 },
                 y: Offset::Pos(antenna2.point.y - antenna.point.y),
             };
+
             let mut top_left = antenna.point.sub_signed(diff);
             while let Some(r_top_left) = top_left {
                 if r_top_left.y < max.y && r_top_left.x < max.x {
@@ -159,6 +141,7 @@ pub fn part2(input: &str) -> String {
                 }
                 top_left = r_top_left.sub_signed(diff);
             }
+
             let mut bot_right = antenna2.point.add_signed(diff);
             while let Some(r_bot_right) = bot_right {
                 if r_bot_right.y < max.y && r_bot_right.x < max.x {
