335f4ea5c886bb688f09b6d03e461a530b9bd6cb
TrojanerHD <github@trojaner.dev>
2024-12-13 14:11:24 +0100

    Remove debug output

--- src/day13.rs
+++ src/day13.rs
@@ -1,12 +1,12 @@
 use std::cmp::Ordering;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 struct Point {
     x: u64,
     y: u64,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 struct Problem {
     a: Point,
     b: Point,
@@ -16,14 +16,11 @@ struct Problem {
 fn process_problems(problems: Vec<Problem>) -> u64 {
     let mut res = 0;
     for problem in problems {
-        println!("Problem: {:?}", problem);
         let a_presses;
         let b_presses;
         match (problem.b.x * problem.a.y).cmp(&(problem.a.x * problem.b.y)) {
             Ordering::Less => {
-                println!("det is less");
                 if problem.a.x * problem.dest.y < problem.a.y * problem.dest.x {
-                    println!("Unsolvable");
                     continue;
                 }
                 b_presses = Some(
@@ -31,15 +28,12 @@ fn process_problems(problems: Vec<Problem>) -> u64 {
                         / (problem.a.x * problem.b.y - problem.b.x * problem.a.y),
                 );
                 if problem.dest.x < problem.b.x * b_presses.unwrap() {
-                    println!("Unsolvable");
                     continue;
                 }
                 a_presses = Some((problem.dest.x - problem.b.x * b_presses.unwrap()) / problem.a.x);
             }
             Ordering::Greater => {
-                println!("det is greater");
                 if problem.b.x * problem.dest.y < problem.b.y * problem.dest.x {
-                    println!("Unsolvable");
                     continue;
                 }
                 a_presses = Some(
@@ -48,28 +42,23 @@ fn process_problems(problems: Vec<Problem>) -> u64 {
                 );
 
                 if problem.dest.x < problem.a.x * a_presses.unwrap() {
-                    println!("Unsolvable");
                     continue;
                 }
                 b_presses = Some((problem.dest.x - problem.a.x * a_presses.unwrap()) / problem.b.x);
             }
             _ => {
-                println!("Unsolvable");
                 continue;
             }
         }
         if a_presses.is_none() || b_presses.is_none() {
-            println!("Unsolvable");
             continue;
         }
         let (a_presses, b_presses) = (a_presses.unwrap(), b_presses.unwrap());
         if problem.a.x * a_presses + problem.b.x * b_presses != problem.dest.x
             || problem.a.y * a_presses + problem.b.y * b_presses != problem.dest.y
         {
-            println!("Unsolvable");
             continue;
         }
-        println!("Final presses: a: {a_presses}, b: {b_presses}");
         res += a_presses * 3 + b_presses;
     }
 
